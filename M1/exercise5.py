e62816cf39ed5a3f404c21a4d1c1423eac81da94
Oliver <oliver.koechli@stud.hslu.ch>
2023-03-02 10:06:48 +0100

    Finished ex5

--- M1/exercise5.py
+++ M1/exercise5.py
@@ -13,21 +13,22 @@ turn_right = 2
 current_position = 0
 
 turn_directions = [1, 1, 2, 2, 2, 2, 2]
-turn_radius = [duration_90_degree,duration_90_degree,3*duration_45_degree,duration_90_degree,duration_90_degree,3*duration_45_degree,3*duration_45_degree]
+turn_radius = [duration_90_degree,duration_90_degree,3*duration_45_degree,duration_90_degree,duration_90_degree-100,3*duration_45_degree,3*duration_45_degree-150]
 drive_durations = [drive_duration,drive_duration,drive_duration,drive_short,drive_short,drive_long,drive_duration,drive_long]
 
 def turn_left(turn_duration):
     global motor_left_target, motor_right_target, default_turn_speed, timer_period,duration_45_degree
     motor_left_target = -default_turn_speed
     motor_right_target = default_turn_speed
-    timer_period[0] = turn_duration
-    print(turn_duration)
+    timer_period[1] = turn_duration
+    timer_period[0] = 32000
     
 def turn_right(turn_duration):
     global motor_left_target, motor_right_target, default_turn_speed, timer_period
     motor_left_target = default_turn_speed
     motor_right_target = -default_turn_speed
-    timer_period[0] = turn_duration
+    timer_period[1] = turn_duration
+    timer_period[0] = 32000
 
 def start():
     global motor_left_target, motor_right_target, default_speed, timer_period, drive_durations
@@ -54,17 +55,26 @@ def buttons():
 def timer0():
     global turn_directions, turn_radius, current_position, timer_period, motor_right_target, motor_left_target
     if current_position <= 7:
-        if motor_right_target != motor_left_target:
+        if motor_right_target == 0 and motor_left_target == 0:
             motor_right_target = default_speed
             motor_left_target = default_speed
             timer_period[0] = drive_durations[current_position]
+            
+            if current_position == 7:
+                current_position = current_position + 1
         else:
             if turn_directions[current_position] == 1:
-                print('left')
                 turn_left(turn_radius[current_position])
             else:
-                print('right')
                 turn_right(turn_radius[current_position])
             current_position = current_position + 1
     else:
         stop()
+        
+@onevent
+def timer1():
+    global motor_left_target, motor_right_target, timer_period
+    motor_left_target = 0
+    motor_right_target = 0
+    timer_period[0] = 500
+    timer_period[1] = 32000
