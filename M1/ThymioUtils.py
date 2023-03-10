cf2dfd3baf1d861c670b69872818a7476c44c536
Timon Kurmann <timon.kurmann@gmail.com>
2023-03-10 09:49:51 +0100

    cleanup

--- M1/ThymioUtils.py
+++ M1/ThymioUtils.py
@@ -1,55 +1,52 @@
 import math
 
 
-class Thymio:
+def mms_to_thymio(v: float) -> int:
+    """
+    Calculate the internal Thymio distance based on the velcocity in mm/s
 
-    @staticmethod
-    def mms_to_thymio(v: float) -> int:
-        """
-        Calculate the internal Thymio distance based on the velcocity in mm/s
+    :param v: velocity in mm/s
+    :return: velocity for Thymio
+    """
+    v_thymio = v * 3.3
+    return min(math.floor(v_thymio), 500)
 
-        :param v: velocity in mm/s
-        :return:
-        """
-        v_thymio = v * 3.3
-        return min(math.floor(v_thymio), 500)
 
-    @staticmethod
-    def curve_velocity(self, r: float, v: float) -> tuple[int, int]:
-        """
-        Calculate velocity for each wheel to drive a certain curve.
+def curve_velocity(r: float, v: float) -> tuple[int, int]:
+    """
+    Calculate velocity for each wheel to drive a certain curve.
 
-        :param r: radius of the curve
-        :param v: velocity in mm/s
-        :return: velocity for each wheel
-        """
-        w = v / r
-        v_inner = w * (r - 105 / 2)
-        v_outer = w * (r + 105 / 2)
-        return self.mms_to_thymio(v_inner), self.mms_to_thymio(v_outer)
+    :param r: radius of the curve
+    :param v: velocity in mm/s
+    :return: Thymio velocity for each wheel
+    """
+    w = v / r
+    v_inner = w * (r - 105 / 2)
+    v_outer = w * (r + 105 / 2)
+    return mms_to_thymio(v_inner), mms_to_thymio(v_outer)
 
-    @staticmethod
-    def time_for_distance(d: int, v: float):
-        """
-        Calculate the time for driving a certain distance at a constant velocity.
 
-        :param d: distance
-        :param v: velocity in mm/s
-        :return: velocity for each wheel
-        """
+def time_for_distance(d: int, v: float):
+    """
+    Calculate the time for driving a certain distance at a constant velocity.
 
-        return math.floor(d / v)
+    :param d: distance
+    :param v: velocity in mm/s
+    :return: time
+    """
 
-    @staticmethod
-    def time_for_circle(self, r, v, degrees=360, error_full_circle=0.3):
-        """
-        Calculate the time for driving a certain circle at a constant velocity.
+    return d / v
 
-        :param r: radius of the circle
-        :param v: velocity in mm/s
-        :param degrees: degrees of circle
-        :param error_full_circle: error term in seconds for a full circle
-        :return: velocity for each wheel
-        """
-        percentage_of_circle = degrees/360
-        return (self.time_for_distance(2 * r * math.pi, v) + error_full_circle) * percentage_of_circle
+
+def time_for_circle(r, v, degrees=360, error_full_circle=0.3):
+    """
+    Calculate the time for driving a certain circle at a constant velocity.
+
+    :param r: radius of the circle
+    :param v: velocity in mm/s
+    :param degrees: degrees of circle
+    :param error_full_circle: error term in seconds for a full circle
+    :return: time
+    """
+    percentage_of_circle = degrees / 360
+    return (time_for_distance(2 * r * math.pi, v) + error_full_circle) * percentage_of_circle
