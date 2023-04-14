c7ac59637a35244586ee2a3078f603c92fcf3869
Timon Kurmann <timon.kurmann@gmail.com>
2023-04-14 10:38:49 +0200

    tablet example

--- H1/exercises/ex7_show_picture_on_tablet.py
+++ H1/exercises/ex7_show_picture_on_tablet.py
@@ -1,21 +1,22 @@
-from pepper_robots import Robot, PepperConfiguration, PepperNames
-from camera import Camera
-from tablet import Tablet
 import time
 
-config = PepperConfiguration(PepperNames.Amber)
+from camera import Camera
+from pepper_robots import Robot, PepperConfiguration, PepperNames
+from tablet import Tablet
+
+config = PepperConfiguration(PepperNames.Pale)
 robot = Robot(config)
 camera = Camera(robot)
 
 # take a picture
-remote_folder_path = "/home/nao/recordings/cameras/"
-file_name = "my_picture.jpg"
+remote_folder_path = "/opt/aldebaran/www/apps/"
+file_name = "my_picture_14_04_2023-1.jpg"
 camera.take_picture(remote_folder_path, file_name)
 
 tablet = Tablet(robot)
 
 try:
-    tablet.show_image(remote_folder_path, file_name)
+    tablet.show_image(file_name)
     time.sleep(5)
     tablet.hide_image()
     tablet.close()
