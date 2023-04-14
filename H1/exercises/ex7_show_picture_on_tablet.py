60fa329de55a4a5b3f7517445e709d13cac5ba14
Timon Kurmann <timon.kurmann@gmail.com>
2023-04-14 10:51:54 +0200

    cleanup

--- H1/exercises/ex7_show_picture_on_tablet.py
+++ H1/exercises/ex7_show_picture_on_tablet.py
@@ -3,6 +3,7 @@ import time
 from camera import Camera
 from pepper_robots import Robot, PepperConfiguration, PepperNames
 from tablet import Tablet
+from file_transfer import FileTransfer
 
 config = PepperConfiguration(PepperNames.Pale)
 robot = Robot(config)
@@ -10,15 +11,19 @@ camera = Camera(robot)
 
 # take a picture
 remote_folder_path = "/opt/aldebaran/www/apps/"
-file_name = "my_picture_14_04_2023-1.jpg"
+file_name = "my_picture_14_04_2023-2.jpg"
 camera.take_picture(remote_folder_path, file_name)
 
+# show image on tablet
 tablet = Tablet(robot)
+tablet.show_image(file_name)
+time.sleep(5)
+tablet.hide_image()
+tablet.close()
 
-try:
-    tablet.show_image(file_name)
-    time.sleep(5)
-    tablet.hide_image()
-    tablet.close()
-except RuntimeError, e:
-    print "error in showing image on tablet", e
+# copy to local filesystem
+local = "C:\\Users\\timon\\Documents\\hslu\\06\\ROBO\\Pepper\\" + file_name
+remote = remote_folder_path + file_name
+file_transfer = FileTransfer(robot)
+file_transfer.get(remote, local)
+file_transfer.close()
--- H1/exercises/tablet.py
+++ H1/exercises/tablet.py
@@ -13,7 +13,7 @@ class Tablet:
         pass
 
     def show_image(self, file):
-        self.__al_tablet_service.showImage("http://198.18.0.1/apps/" + file)
+        self.__al_tablet_service.showImageNoCache("http://198.18.0.1/apps/" + file)
 
     def hide_image(self):
         self.__al_tablet_service.hideImage()
