8e3208a6188a5a7528698d1b768937195c6fea72
Oliver <oliver.koechli@stud.hslu.ch>
2023-04-14 11:17:41 +0200

    started dialog with pepper

--- H1/exercises/dialog.py
+++ H1/exercises/dialog.py
@@ -4,39 +4,57 @@ import time
 class Dialog:
 
     def __init__(self, robot):
+        self.__al_mem = robot.ALMemory
+        self.subscriber = self.__al_mem.subscriber('WordRecognized')
+        self.subscriber.signal.connect(self.callback)
         self.__al_tts = robot.ALTextToSpeech
+        self.__al_sr = robot.ALSpeechRecognition
+        self.topics = {}
 
     def say(self, text):
         self.__al_tts.say(text)
 
     def say_slowly(self, text):
-        # to be implemented
-        pass
+        self.__al_tts.setParameter('speed', 50)
+        self.__al_tts.say(text)
+        self.__al_tts.setParameter('speed', 100)
 
     def shout(self, text):
-        # to be implemented
-        pass
+        oldVolume = self.__al_tts.getVolume()
+        self.__al_tts.setVolume(2)
+        self.__al_tts.say(text)
+        self.__al_tts.setVolume(oldVolume)
 
-    def add_simple_reaction(self, user_input, robot_output):
-        # to be implemented
-        pass
+    def add_simple_reaction(self, topic_name, user_input, robot_output):
+        self.topics[topic_name].append({
+            'input': user_input,
+            'output': robot_output
+        })
+        self.__al_sr.setVocabulary(user_input.split(), False)
 
     def load_yes_no_question(self, question, reaction_yes, reaction_no):
         # to be implemented
         pass
 
     def ask_yes_no_question(self, topic):
-        # to be implemented
         pass
+
+    def add_topic(self, topic_name):
+        self.topics[topic_name] = []
 
     def start_topic(self, topic_name):
-        # to be implemented
-        pass
+        self.__al_sr.subscribe(topic_name, 1, 0.2)
 
     def stop_topic(self, topic_name):
-        # to be implemented
-        pass
+        self.__al_sr.unsubscribe(topic_name)
 
     def close_session(self):
-        # to be implemented
-        pass
+        self.topics = {}
+
+    def callback(self, value):
+        print(value)
+
+        for topic, array in self.topics.items():
+            for input in array:
+                if value[0] == input['input'] and value[1] > 0.35:
+                    self.say(input['output'])
--- H1/exercises/ex3_simple_interaction.py
+++ H1/exercises/ex3_simple_interaction.py
@@ -2,11 +2,12 @@ from pepper_robots import Robot, PepperConfiguration, PepperNames
 from dialog import Dialog
 import time
 
-config = PepperConfiguration(PepperNames.Amber)
+config = PepperConfiguration(PepperNames.Pale)
 robot = Robot(config)
 dialog = Dialog(robot)
 
 topic_name = "introduction"
+dialog.add_topic(topic_name)
 dialog.add_simple_reaction(topic_name, "hello", "hello human, pleased to meet you")
 dialog.start_topic(topic_name)
 print "time to interact with pepper"
