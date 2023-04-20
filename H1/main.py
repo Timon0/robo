941c0aae261c0f6bbf4245bfca75ebdb1e3643cd
Timon Kurmann <timon.kurmann@gmail.com>
2023-04-20 10:55:19 +0200

    cleanup

--- H1/main.py
+++ H1/main.py
@@ -5,9 +5,17 @@ from pepper_robots import Robot, PepperConfiguration, PepperNames
 config = PepperConfiguration(PepperNames.Pale)
 robot = Robot(config)
 image_recognition = ImageRecognition()
-
 i_see_pictures = ISeePictures(robot)
+
+# TODO: Welcome dialog
+
 picture_locations = i_see_pictures.take_pictures()
+selected_object, selected_object_parent = image_recognition.get_object_from_selection(picture_locations)
+
+# TODO: Guessing dialog
+
+# TODO: Maybe add hint if parent of object is available
 
 print(picture_locations)
-print(image_recognition.get_object_from_selection(picture_locations))
+print(selected_object)
+print(selected_object_parent)
