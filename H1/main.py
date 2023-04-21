aa5291489c23c6d901d38818676d5a9137b3aa4f
Timon Kurmann <timon.kurmann@gmail.com>
2023-04-21 09:57:47 +0200

    cleanup

--- H1/main.py
+++ H1/main.py
@@ -9,24 +9,26 @@ robot = Robot(config)
 image_recognition = ImageRecognition()
 i_see_pictures = ISeePictures(robot)
 
+# person identification
+robot.ALBasicAwareness.startAwareness()
+
 # TODO: Welcome dialog
-robot.ALAutonomousMoves.startSmallDisplacements()
 robot.ALTextToSpeech.say("Welcome to the game!")
-print(robot.ALBasicAwareness.getTrackingMode())
-print(robot.ALBasicAwareness.isAwarenessRunning())
 time.sleep(10)
 
-# TODO: Person identification
 
-robot.ALAutonomousMoves.stopSmallDisplacements()
+# take pictures
+robot.ALBasicAwareness.stopAwareness()
+robot.ALTextToSpeech.say("Wait a second while I scan the room for some objects.")
 picture_locations = i_see_pictures.take_pictures()
 selected_object, selected_object_parent = image_recognition.get_object_from_selection(picture_locations)
 
-# TODO: Guessing dialog
-# TODO: Maybe add hint if parent of object is available
+# todo: guessing dialog
+
+# todo: maybe add hint if parent of object is available
 
 
-# TODO: Want to Play again?
+# todo: want to play again?
 
 print(picture_locations)
 print(selected_object)
