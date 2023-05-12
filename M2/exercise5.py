144767bda262029de1401c57e9caba8ef3fc3a1a
Oliver <oliver.koechli@stud.hslu.ch>
2023-05-12 11:26:07 +0200

    finished ex5

--- M2/exercise5.py
+++ M2/exercise5.py
@@ -1,4 +1,4 @@
-default_speed = 200
+default_speed = 400
 started = False
 
 error = 0
@@ -6,21 +6,33 @@ prev_error = 0
 
 error_sum = 0
 
-c_p = 20
-c_d = 5
-c_i = 5
+prev_error_size = 10
+prev_errors = [0,0,0,0,0,0,0,0,0,0]
+current_pos = 0
 
-black = 800
+c_p = 35
+c_d = 5
+c_i = 15
 
 def drive_straigth(speed):
     global motor_left_target, motor_right_target
     motor_left_target = speed
     motor_right_target = speed
+    
+def fill_errors(error_size):
+    pass
+
+def update_errors(current_error):
+    prev_errors[current_pos] = current_error
+    current_pos += 1
+    if current_pos == 10:
+        current_pos = 0
 
 @onevent
 def button_forward():
-    global defaultSpeed, started
+    global defaultSpeed, started, prev_error_size
     drive_straigth(default_speed)
+    #prev_errors = fill_errors(prev_error_size)
     started = True
     
 @onevent
@@ -32,35 +44,25 @@ def button_backward():
 
 @onevent
 def prox():
-    global motor_right_target, motor_left_target, default_speed, started, error, prev_error, error_sum, black, c_p, c_i, c_d
+    global motor_right_target, motor_left_target, default_speed, started, error, prev_error, black, c_p, c_i, c_d, prev_errors, prev_error_size
     if started:
         p_right = prox_ground_delta[1]
         p_left = prox_ground_delta[0]
         
-        position = black
-        if p_right < black:
-            position = p_right + black
-        elif p_left < black:
-            position = p_left
+        error = p_right - p_left
         
-        error = black - position
+        update_errors(error)
+        summe = 0
+        print(len(prev_errors))
+        for i in range(len(prev_errors)):
+            summe += prev_errors[i]
+            print(prev_errors[i])
+        error_sum = summe // prev_error_size
         
-        error_sum += error
-
         P = (c_p * (error)) // 100
-        I = (c_i * error_sum) // 300
+        I = (c_i * error_sum) // 400
         D = (c_d * (error - prev_error)) // 300
         change = P + I + D
-        
-        if  error_sum == 0:
-            updated_ground_speed = default_speed
-        
-        if  abs(error_sum // 100) == 0:
-            updated_ground_speed = default_speed
-        else:
-            speed = (1000 * default_speed)
-            abs_error = (abs(error_sum) // 100)
-            updated_ground_speed = speed // abs_error
         
         motor_left_target = default_speed - change
         motor_right_target = default_speed + change
